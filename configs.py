8abec4ae72899c5706c22c201eeee6d14ca09193
ak4422920 <ak4422920@gmail.com>
2025-03-18 23:04:51 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import getenv as genv
 API_ID = genv("API_ID", "29171167")
 API_HASH = genv("API_HASH", "7ea2149629e445936619f06a3c0dc716")
 BOT_TOKEN = genv("BOT_TOKEN", "")
-BASE_URL = genv("BASE_URL", "")
+BASE_URL = genv("BASE_URL", "tricky-dede-akad-e2b2d353.koyeb.app")
 SUPPORT_GROUP = genv("SUPPORT_GROUP", "Cineoriginals")
 UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "Cineoriginals")
 DATABASE_URL = genv("DATABASE_URL", "mongodb+srv://akre:akre@cluster0.gtpo3kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
