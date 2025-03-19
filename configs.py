b34419641be75517d0470e13227a46177336c87c
Techify Bots <TechifyBots@gmail.com>
2025-03-19 06:26:55 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,11 +7,7 @@ API_ID = int(os.getenv("API_ID", 29171167))
 API_HASH = os.getenv("API_HASH", "7ea2149629e445936619f06a3c0dc716")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 BASE_URL = os.getenv("BASE_URL", "tricky-dede-akad-e2b2d353.koyeb.app")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "Cineoriginals")
-UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "Cineoriginals")
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://akre:akre@cluster0.gtpo3kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-AUTH_CHANNELS = os.getenv("AUTH_CHANNEL", "-100****, -100****")
-AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")]
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100****"))
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.getenv("ADMINS", "123456").split(",")]
 
